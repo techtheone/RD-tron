af7c3be3c9d76ef451bc1e07a1f0c725183c9d37
techtheone <tech@theone.pro>
2021-07-05 16:48:36 +0800

    update

--- SafeMath.sol
+++ SafeMath.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.25;
 
 library SafeMath {
   function mul(uint256 a, uint256 b) internal pure returns (uint256) {
@@ -6,23 +6,27 @@ library SafeMath {
       return 0;
     }
     uint256 c = a * b;
-    assert(c / a == b);
+    //assert(c / a == b);
+    require(c / a == b, "SafeMath: multiplication overflow");
     return c;
   }
 
   function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b > 0, "SafeMath: division by zero");
     uint256 c = a / b;
     return c;
   }
 
   function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
+    //assert(b <= a);
+    require(b <= a, "SafeMath: subtraction overflow");
     return a - b;
   }
 
   function add(uint256 a, uint256 b) internal pure returns (uint256) {
     uint256 c = a + b;
-    assert(c >= a);
+    //assert(c >= a);
+    require(c >= a, "SafeMath: addition overflow");
     return c;
   }
 }
